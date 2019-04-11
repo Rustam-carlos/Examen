0e37ab16913c235e6f282673f72dda08495deb3a
Rustam Galeev <Vincent-cat@mail.ru>
2019-04-12 00:58:54 +0600

    Добавьте файлы проекта.

---
+++ Examen/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Examen
+{
+    [Serializable]
+    public class Product
+    {
+        public string Prod_name { get; set; }
+        public string Manufact { get; set; }
+        public int Prod_cnt { get; set; }
+
+        public Product()
+        {
+            Prod_name = "";
+            Manufact = "Unknow";
+            Prod_cnt = 0;
+        }
+        public Product(string name, string Autor, int count)
+        {
+            Prod_name = name;
+            Manufact = Autor;
+            Prod_cnt = count;
+        }
+
+        public delegate void Add_product();
+        public event Add_product Press_ctr;
+
+        public void Action()
+        {
+            Console.WriteLine("Введите название продукта: ");
+            Prod_name = Console.ReadLine();
+            Console.WriteLine("Введите название производителя продукта: ");
+            Manufact = Console.ReadLine();
+            Console.WriteLine("Введите колличество продукта: ");
+            Prod_cnt = int.Parse(Console.ReadLine());
+            Show();
+        }
+        public void Show()
+        {
+            Console.WriteLine("Название продукта: " + Prod_name);
+            Console.WriteLine("Производитель продукта: " + Manufact);
+            Console.WriteLine("Колличество продукта: " + Prod_cnt);
+        }
+    }
+}
---
+++ Examen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            Console.WriteLine("Добро пожаловать в программу");
+            Console.WriteLine("Нажмите клавишу пробел для добавления продукта");
+            var keyinfo = Console.ReadKey();
+
+            if (Console.ReadKey(true).Key == ConsoleKey.Spacebar)
+            {
+                //Console.Write("CTL+");
+                product.Action();
+            }
+            else
+                Console.Write("Нажата неверная клавиша");
+
+            XmlSerializer sr = new XmlSerializer(typeof(Product));
+            string path = (@"C:\Prod.xml");
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                sr.Serialize(fs, product);
+            }
+        }
+    }
+}
